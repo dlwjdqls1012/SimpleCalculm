70a557278b492eae93db5f54e5759d3af0cea057
dlwjdqls1012 <dlwjdqls1356@gmail.com>
2024-04-04 15:14:16 +0900

    계산기

--- app/src/main/java/kr/ac/kopo/simplecalculm/MainActivity.kt
+++ app/src/main/java/kr/ac/kopo/simplecalculm/MainActivity.kt
@@ -17,18 +17,20 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnDivide:Button
     lateinit var btnMod:Button
     lateinit var textResult:TextView
-    var result : Int? = null
+    var result : Double? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        위젯 객체의 참조값을 필드로 선언된 참조변수에 대입
         edit1 = findViewById<EditText>(R.id.edit1)
         edit2 = findViewById<EditText>(R.id.edit2)
-        btnPlus = findViewById(R.id.btnPlus)
-        btnMinus = findViewById(R.id.btnMinus)
-        btnMultiply = findViewById(R.id.btnMultiply)
-        btnDivide = findViewById(R.id.btnDivide)
-        btnMod = findViewById(R.id.btnMod)
+        btnPlus = findViewById<Button>(R.id.btnPlus)
+        btnMinus = findViewById<Button>(R.id.btnMinus)
+        btnMultiply = findViewById<Button>(R.id.btnMultiply)
+        btnDivide = findViewById<Button>(R.id.btnDivide)
+        btnMod = findViewById<Button>(R.id.btnMod)
         textResult = findViewById<TextView>(R.id.textResult)
 
         btnPlus.setOnClickListener(btnListener)
@@ -36,31 +38,35 @@ class MainActivity : AppCompatActivity() {
         btnMultiply.setOnClickListener(btnListener)
         btnDivide.setOnClickListener(btnListener)
         btnMod.setOnClickListener(btnListener)
+
     }
+
     var btnListener = OnClickListener{
         val num1Str = edit1.text.toString()
         val num2Str = edit2.text.toString()
 
-        if(num1Str==""||num1Str==""){
-            Toast.makeText(baseContext, "입력을 하지 않았습니다.", Toast.LENGTH_SHORT).show()
+        if(num1Str=="" || num1Str==""){
+            Toast.makeText(baseContext, "입력을 하지 않았습니다.", Toast.LENGTH_LONG).show()
             return@OnClickListener
         }
 
         val num1 = num1Str.toDouble()
         val num2 = num2Str.toDouble()
 
-        when(it.id) {
+        when(it.id){
             R.id.btnPlus -> result = num1 + num2
             R.id.btnMinus -> result = num1 - num2
             R.id.btnMultiply -> result = num1 * num2
-            R.id.btnDivide -> {
+            R.id.btnDivide ->{
                 if(num2 == 0.0){
                     Toast.makeText(baseContext, "0으로 나누면 안됩니다.", Toast.LENGTH_LONG).show()
+                    return@OnClickListener
+                }
+                result = num1 / num2
             }
-            result = num1 / num2
-        }
             R.id.btnMod -> result = num1 % num2
         }
-        textResult.text = "계산결과: " + result
+
+        textResult.text = "계산 결과: " + result
     }
 }
